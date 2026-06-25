3a68444e20719d05cb1952570defc02e7b44859b
latvianlazer77 <zacgra62@kingshigh.school.nz>
2026-06-26 10:53:15 +1200

    Worked on the skull spiders a bit

--- Scripts/spider.gd
+++ Scripts/spider.gd
@@ -7,27 +7,37 @@ var player: Node3D = null
 var active := false
 
 func _ready():
-	# Find the player in the scene
+	# Shrink the spider
+	scale = Vector3(0.2, 0.2, 0.2)
+	
+	# Find the player
 	var players = get_tree().get_nodes_in_group("player")
 	if players.size() > 0:
 		player = players[0]
+	else:
+		print("CRITICAL: Spider can't find any node in the 'player' group!")
+
+	# SAFETY NET: Keep the killzone disabled for 0.5 seconds 
+	# to completely stop the single-frame glitch death.
+	if has_node("KillZone/CollisionShape3D"):
+		$KillZone/CollisionShape3D.disabled = true
+		await get_tree().create_timer(0.5).timeout
+		$KillZone/CollisionShape3D.disabled = false
 
 func _physics_process(_delta):
 	if not active or not player:
 		return
 		
-	# Target the player's position through the maze navigation mesh
+	# Update pathfinding target
 	nav_agent.target_position = player.global_position
 	
 	if not nav_agent.is_navigation_finished():
 		var next_path_pos = nav_agent.get_next_path_position()
 		var direction = (next_path_pos - global_position).normalized()
 		
-		# Move the spider
 		velocity = direction * speed
 		move_and_slide()
 		
-		# Make the spider face where it's running
 		var look_dir = direction
 		look_dir.y = 0
 		if look_dir.length() > 0.1:
@@ -35,5 +45,3 @@ func _physics_process(_delta):
 
 func start_chase():
 	active = true
-	# If you have an AnimationPlayer, play your walk animation here:
-	# $AnimationPlayer.play("walk")
